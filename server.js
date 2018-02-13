77493446b2f50934651ab166c2d02585aed50be4
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-02-13 15:37:57 -0500

    needs correct return object

--- server.js
+++ server.js
@@ -1,54 +1,53 @@
 // node/express app starts here!
 
+// const querystring = require('querystring')
 const express = require('express')
-const bodyParser = require('body-parser')
-// const multer  = require('multer') //reinstall multer package, then uncomment to handle multipart/form-data content-type
-// const upload = multer()
+const moment = require('moment')
+// const bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: false })); //https://github.com/expressjs/body-parser
 
-app.get("/", function (request, response) {
-  console.log('get request to /')
-  // response.send('<p>Hello World!</p>')
-  // response.download('./README.md')
-  response.sendFile(__dirname + '/views/index.html')
-})
+//this middleware is acting as an endpoint that handles all requests to server.
+app.all('*', (req,res,next)=>{
+  let unixTimestamp, naturalDate
+  let inputStr = req.originalUrl.slice(1).replace(/%20/g,' ')
 
-//pug template rendering
-app.set('view engine', 'pug')
-app.set('views', __dirname + '/views' )
-app.get('/pugrender', (req,res)=>{
-  console.log('get request to /pugrender')
-  //res.render accepts a template filename (like the pug file) and data called locals
-  res.render('index', {date: new Date().toDateString()} )
-})
+  //create a local moment from a Unix timestamp (seconds since the Unix Epoch),convert to utc, then format display
+  // console.log( moment.unix('1450137600').utc().format('MMMM Do, YYYY') )
+  // console.log( moment.unix(inputStr).utc().format('MMMM Do, YYYY') )
 
-app.get("/dreams", function (request, response) {
-  console.log('get request to /dreams')
-  response.send(dreams)
-})
-// Simple in-memory store for now
-let dreams = [ "Find and count some sheep", "Climb a really tall mountain", "Wash the dishes" ]
+  //check if the provided string will return a valid 'moment' date when parsed
+  if( !moment.unix(inputStr).isValid() && !moment(inputStr,
+    [
+      'M-D-YY','M D YY',
+      'MMM-D-YY','MMM D YY',
+      'MMMM-D-YY','MMMM D YY',
+      'M-D-YYYY','M D YYYY',
+      'MMM-D-YYYY','MMM D YYYY',
+      'MMMM-D-YYYY','MMMM D YYYY'
+    ],true)
+    .isValid() ) {
+    res.send('server did not receive a date')
+  }
+  else {
+    //set values for properties in the response object
+    unixTimestamp = moment.unix(inputStr).utc().unix()
+    //accepts string dates as month-day-year delimited by spaces or hyphens
+    naturalDate = moment.utc(inputStr,
+      [
+        'M-D-YY','M D YY',
+        'MMM-D-YY','MMM D YY',
+        'MMMM-D-YY','MMMM D YY',
+        'M-D-YYYY','M D YYYY',
+        'MMM-D-YYYY','MMM D YYYY',
+        'MMMM-D-YYYY','MMMM D YYYY'
+      ],true)
+      .format('MMMM Do, YYYY')
+    //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
+    res.send( { unix:unixTimestamp, natural:naturalDate } )
+  }
 
-// //multer route to handle content-type multipart/form-data. form field values in request.body
-// app.post('/dreams', upload.array(), (request, response, next)=> {
-//   console.log('post request to /dreams')
-//   console.log(request.body)
-//   // console.log(request.headers)
-//   response.send("server received all data from POST request")
-  
-// })
-
-app.use( bodyParser.json() )//for parsing application/json body content-types, will add the value to request.body
-
-app.post("/dreams", (request, response)=> {
-  console.log('post request to /dreams')
-  // console.log(request.headers)
-  console.log(request.body)
-  response.send("server received all data from POST request")
-  //push item to store, so page refresh shows what user has inputted so far
-  dreams.push(request.body.todo)
 })
 
 // listen for requests. uncomment line depending on server. choose first for glitch, as the .env files with the port will become available when their site hosts the app. or use the second when running this app locally
