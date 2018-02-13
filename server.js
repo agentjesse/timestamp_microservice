09a8e3d6964340691c5a996c58748387ae0eebd5
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-02-13 17:40:29 -0500

    works

--- server.js
+++ server.js
@@ -13,34 +13,14 @@ app.all('*', (req,res,next)=>{
   //create a local moment from a Unix timestamp (seconds since the Unix Epoch) from str or num, use utc mode, then format display
   // console.log( moment.unix('1450137600').utc().format('MMMM Do, YYYY') )
 
-  //check if the input is actually a date in either format
-  if( !moment.unix(inputStr).isValid() && !moment(inputStr,
-    [
-      'M-D-YY','M D YY',
-      'MMM-D-YY','MMM D YY',
-      'MMMM-D-YY','MMMM D YY',
-      'M-D-YYYY','M D YYYY',
-      'MMM-D-YYYY','MMM D YYYY',
-      'MMMM-D-YYYY','MMMM D YYYY'
-    ],true)
-    .isValid() ) {
+  //show launchpage for empty input
+  if( req.originalUrl === '/' ) {
     //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
-    res.send( { unix:null, natural:null } )
+    res.send( "In the url after a '/', give me a unix timestamp or month-day-year date delimited by spaces or hyphens" )
   }
-  //the input is valid, respond accordingly
-  else {
-    // check if the input was a number, signed or not
-    if ( /^-?[0-9]+$/.test(inputStr) ) {
-      // console.log('received unix timestamp: ', inputStr)
-      //set vars accordingly, to use in response object
-      unixTimestamp = moment.unix(inputStr).utc().unix() //you could also just display as a string directly
-      naturalDate = moment.unix(inputStr).utc().format('MMMM Do, YYYY')
-    }
-    //received a non unix date...
-    else{
-      //set vars accordingly, to use in response object
-      //accepts string dates as month-day-year delimited by spaces or hyphens
-      naturalDate = moment.utc(inputStr,
+  else{
+    //check if the input is actually a date in either format
+    if( !moment.unix(inputStr).isValid() && !moment(inputStr,
       [
         'M-D-YY','M D YY',
         'MMM-D-YY','MMM D YY',
@@ -49,8 +29,24 @@ app.all('*', (req,res,next)=>{
         'MMM-D-YYYY','MMM D YYYY',
         'MMMM-D-YYYY','MMMM D YYYY'
       ],true)
-      .format('MMMM Do, YYYY')
-      unixTimestamp = moment.utc(inputStr,
+      .isValid() ) {
+      //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
+      res.send( { unix:null, natural:null } )
+    }
+    //the input is valid, respond accordingly
+    else {
+      // check if the input was a number, signed or not
+      if ( /^-?[0-9]+$/.test(inputStr) ) {
+        // console.log('received unix timestamp: ', inputStr)
+        //set vars accordingly, to use in response object
+        unixTimestamp = moment.unix(inputStr).utc().unix() //you could also just display as a string directly
+        naturalDate = moment.unix(inputStr).utc().format('MMMM Do, YYYY')
+      }
+      //received a non unix date...
+      else{
+        //set vars accordingly, to use in response object
+        //accepts string dates as month-day-year delimited by spaces or hyphens
+        naturalDate = moment.utc(inputStr,
         [
           'M-D-YY','M D YY',
           'MMM-D-YY','MMM D YY',
@@ -58,12 +54,24 @@ app.all('*', (req,res,next)=>{
           'M-D-YYYY','M D YYYY',
           'MMM-D-YYYY','MMM D YYYY',
           'MMMM-D-YYYY','MMMM D YYYY'
-        ],true).unix()
-
+        ],true)
+        .format('MMMM Do, YYYY')
+        unixTimestamp = moment.utc(inputStr,
+          [
+            'M-D-YY','M D YY',
+            'MMM-D-YY','MMM D YY',
+            'MMMM-D-YY','MMMM D YY',
+            'M-D-YYYY','M D YYYY',
+            'MMM-D-YYYY','MMM D YYYY',
+            'MMMM-D-YYYY','MMMM D YYYY'
+          ],true).unix()
+  
+      }
+  
+      //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
+      res.send( { unix:unixTimestamp, natural:naturalDate } )
     }
 
-    //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
-    res.send( { unix:unixTimestamp, natural:naturalDate } )
   }
 
 })
