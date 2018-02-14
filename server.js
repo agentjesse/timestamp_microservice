1c3534d00de657a66f487ee86145e9f7cf162f0b
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-02-14 14:27:39 -0500

    final

--- server.js
+++ server.js
@@ -2,7 +2,15 @@
 
 const express = require('express')
 const moment = require('moment')
+path = require('path')
 const app = express()
+
+//specify template engine for express to use
+app.set('view engine', 'pug')
+//specify a PATH for template files. fallback to yours if none provided as cmd-line arguments. join() just returns appropriate win/unix path 
+app.set('views', path.join(__dirname, 'views') )
+//host css files. they will appear at server root with same level where index.html is hosted
+app.use( express.static( path.join(__dirname,'public') ) )
 
 //this middleware handles all requests to server. can also just use app.use(fn)
 app.all('*', (req,res,next)=>{
@@ -14,13 +22,12 @@ app.all('*', (req,res,next)=>{
   // console.log( moment.unix('1450137600').utc().format('MMMM Do, YYYY') )
 
   //show launchpage for empty input
-  if( req.originalUrl === '/' ) {
-    //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
-    res.send( "In the url after a '/', give me a unix timestamp or month-day-year date delimited by spaces or hyphens" )
-  }
+  //res.render accepts a template filename (like the pug file) and data called locals
+  if( req.originalUrl === '/' ) res.render('index')
   else{
-    //check if the input is actually a date in either format
-    if( !moment.unix(inputStr).isValid() && !moment(inputStr,
+    //make both moment objects from the input. you can check them later using isValid() and display them
+    //accepts string dates as month-day-year delimited by spaces or hyphens
+    naturalDate = moment.utc(inputStr,
       [
         'M-D-YY','M D YY',
         'MMM-D-YY','MMM D YY',
@@ -29,45 +36,28 @@ app.all('*', (req,res,next)=>{
         'MMM-D-YYYY','MMM D YYYY',
         'MMMM-D-YYYY','MMMM D YYYY'
       ],true)
-      .isValid() ) {
-      //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
+    // console.log('date from natural format parser',naturalDate)
+    unixTimestamp = moment.unix(inputStr).utc()
+    // console.log('date from unix format parser',unixTimestamp)
+
+    //check if the moment objects are valid dates....
+    if( !naturalDate.isValid() && !unixTimestamp.isValid() )
       res.send( { unix:null, natural:null } )
-    }
-    //the input is valid, respond accordingly
+    //now we know at least one of the inputs is a valid date. respond accordingly
     else {
-      // check if the input was a number, signed or not
-      if ( /^-?[0-9]+$/.test(inputStr) ) {
-        // console.log('received unix timestamp: ', inputStr)
-        //set vars accordingly, to use in response object
-        unixTimestamp = moment.unix(inputStr).utc().unix() //you could also just display as a string directly
-        naturalDate = moment.unix(inputStr).utc().format('MMMM Do, YYYY')
+      // check if the input was a unix timestamp, signed or not, then set vars from unix timestamp moment
+      // if ( /^-?[0-9]+$/.test(inputStr) ) {
+      if ( unixTimestamp.isValid() ) {
+        naturalDate = unixTimestamp.clone()
+        unixTimestamp = unixTimestamp.unix() //this moment object must be valid, so use it for display
+        naturalDate = naturalDate.format('MMMM Do, YYYY')
       }
-      //received a non unix date...
+      //if input was a natural date instead, set vars from natural date moment
       else{
-        //set vars accordingly, to use in response object
-        //accepts string dates as month-day-year delimited by spaces or hyphens
-        naturalDate = moment.utc(inputStr,
-        [
-          'M-D-YY','M D YY',
-          'MMM-D-YY','MMM D YY',
-          'MMMM-D-YY','MMMM D YY',
-          'M-D-YYYY','M D YYYY',
-          'MMM-D-YYYY','MMM D YYYY',
-          'MMMM-D-YYYY','MMMM D YYYY'
-        ],true)
-        .format('MMMM Do, YYYY')
-        unixTimestamp = moment.utc(inputStr,
-          [
-            'M-D-YY','M D YY',
-            'MMM-D-YY','MMM D YY',
-            'MMMM-D-YY','MMMM D YY',
-            'M-D-YYYY','M D YYYY',
-            'MMM-D-YYYY','MMM D YYYY',
-            'MMMM-D-YYYY','MMMM D YYYY'
-          ],true).unix()
-  
+        unixTimestamp = naturalDate.clone()
+        naturalDate = naturalDate.format('MMMM Do, YYYY')
+        unixTimestamp = unixTimestamp.unix()
       }
-  
       //respond with an object as a json encoded string. send() is smart enough to handle it instead of res.json()
       res.send( { unix:unixTimestamp, natural:naturalDate } )
     }
